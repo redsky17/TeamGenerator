7b5a6cd75a8610c34dd141c07f2924ffb3e25d32
Joseph Donofry <Rubberduckie3554@gmail.com>
2012-12-13 14:51:05 -0500

    Added preliminary bracket support

--- TeamGenerator/Program.cs
+++ TeamGenerator/Program.cs
@@ -8,6 +8,30 @@ namespace TeamGenerator
 {
     class Program
     {
+        class Bracket
+        {
+            Random rand = new Random();
+
+            public List<Team> teams = new List<Team>();
+
+            public List<Team> FisherYatesShuffle(List<Team> teamList)
+            {
+                List<Team> retArray = new List<Team>(teamList);
+
+                for (int i = teams.Count - 1; i > 0; i--)
+                {
+                    int j = rand.Next(0, i) + 1;
+
+                    Team temp = retArray[i];
+                    retArray[i] = retArray[j];
+                    retArray[j] = temp;
+
+                }
+
+                return retArray;
+            }
+        }
+
         class Team
         {
             public List<string> players = new List<string>();
@@ -78,6 +102,7 @@ namespace TeamGenerator
                     {
                         List<string> names = new List<string>();
                         List<Team> teams = new List<Team>();
+                        Bracket bracket = new Bracket();
                         string line;
                         while ((line = reader.ReadLine()) != null)
                         {
@@ -110,6 +135,8 @@ namespace TeamGenerator
                                     writer.WriteLine(tempTeam.ToString());
 
                                     lastIndex = i;
+
+                                    teams.Add(tempTeam);
                                 }
 
                                 Team lastTeam = new Team();
@@ -124,6 +151,7 @@ namespace TeamGenerator
 
                                 }
 
+
                                 if (lastTeam.players.Count > 0)
                                 {
 #if DEBUG
@@ -131,11 +159,61 @@ namespace TeamGenerator
 #endif
 
                                     writer.WriteLine(lastTeam.ToString());
+
+
+                                    teams.Add(lastTeam);
                                 }
 
-#if !DEBUG
-                                Console.WriteLine("Teams saved to " + teamsFile);
+
+                                bracket.teams = bracket.FisherYatesShuffle(teams);
+
+                         
+                                writer.WriteLine();
+                                writer.WriteLine("Team Matchups: ");
+
+
+#if DEBUG
+                                Console.WriteLine();
+                                Console.WriteLine("Team Matchups: ");
 #endif
+
+                                for (int i = 0; i < bracket.teams.Count - 1; i += 2)
+                                {
+#if DEBUG
+                                    Console.WriteLine(string.Format("{0, 30} vs {1, 30}", bracket.teams[i].teamName, bracket.teams[i + 1].teamName));
+#endif
+                                    writer.WriteLine(string.Format("{0, 30} vs {1, 30}", bracket.teams[i].teamName, bracket.teams[i + 1].teamName));
+
+                                    lastIndex = i;
+                                    
+                                }
+
+
+                                if (lastIndex + 2 < bracket.teams.Count)
+                                {
+                                    writer.WriteLine();
+                                    writer.WriteLine("Teams starting with a bye: ");
+
+
+#if DEBUG
+                                    Console.WriteLine();
+                                    Console.WriteLine("Teams starting with a bye: ");
+#endif
+
+                                    for (int i = lastIndex + 2; i < bracket.teams.Count; i++)
+                                    {
+#if DEBUG
+                                        Console.WriteLine(bracket.teams[i].teamName);
+#endif
+                                        writer.WriteLine(bracket.teams[i].teamName);
+                                    }
+                                }
+#if !DEBUG
+                                    
+                                Console.WriteLine("Teams saved to " + teamsFile);
+
+#endif
+
                             }
                         }
                     }
